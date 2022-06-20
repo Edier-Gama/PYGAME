9bf6c440ee59b277e2498c2dc4fc39cf32dcf6e9
Edier Gama <ediersantiago7@gmail.com>
2022-06-20 00:58:12 -0500

    primer boceto del juego 100% terminado

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ SCREEN = pygame.display.set_mode((900, 700))
 pygame.display.set_caption("Menu")
 
 BG = pygame.image.load("assets/fondo.jpg")
+FG = pygame.image.load("assets/fondo_ganador.jpg")
+FP = pygame.image.load("assets/fondo_perdedor.jpg")
+
 
 def get_font(size): 
     return pygame.font.Font("assets/font.ttf", size)
@@ -345,16 +348,16 @@ def options():
 
 def menu_final():
     while True:
-        SCREEN.blit(BG, (0, 0))
+        SCREEN.blit(FP, (0, 0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(35).render("HAS PERDIDO:( TUS PUNTOS NO DAN", True, "black")
+        MENU_TEXT = get_font(35).render("HAS PERDIDO:( TUS PUNTOS NO DAN", True, "white")
         MENU_RECT = MENU_TEXT.get_rect(center=(400, 30))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(400, 250), 
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(400, 300), 
                             text_input="JUGAR DE NUEVO", font=get_font(35), base_color="black", hovering_color="green")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(400, 350), 
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(400, 420), 
                             text_input="SALIR", font=get_font(35), base_color="black", hovering_color="red")
 
         CREDITOS = Button(image=None, pos=(400, 650),text_input="Gloria Gama, Jeyson Olivare, Julian", font=get_font(20), base_color="white", hovering_color="Green")
@@ -381,7 +384,7 @@ def menu_final():
         
 def menu_final_ganador():
     while True:
-        SCREEN.blit(BG, (0, 0))
+        SCREEN.blit(FG, (0, 0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
