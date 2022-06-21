fd5a759dfed48c9194194a65b867fab3ecff7ab7
Edier Gama <ediersantiago7@gmail.com>
2022-06-20 22:44:08 -0500

    JUEGO FINALIZADO

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ pygame.display.set_caption("Menu")
 BG = pygame.image.load("assets/fondo.jpg")
 FG = pygame.image.load("assets/fondo_ganador.jpg")
 FP = pygame.image.load("assets/fondo_perdedor.jpg")
+FE = pygame.image.load("assets/fondo_empate.jpg")
 
 
 def get_font(size): 
@@ -274,7 +275,9 @@ def play():
     if score > score_bot and vidas <= 0:
         menu_final_ganador()
     if score == score_bot and vidas <= 0:
-        menu_final_empate()    
+        menu_final_empate()
+    if score_bot > score and mechaLista == 0:
+        menu_final()        
                 
     # Animacion principal de la mano
     cord_x += speed_x
@@ -421,17 +424,17 @@ def menu_final_ganador():
 
 def menu_final_empate():
     while True:
-        SCREEN.blit(BG, (0, 0))
+        SCREEN.blit(FE, (0, 0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
         MENU_TEXT = get_font(35).render("ESTO HA SIDO UN EMPATE :0", True, "black")
         MENU_RECT = MENU_TEXT.get_rect(center=(400, 30))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(400, 250), 
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(200, 540), 
                             text_input="JUGAR DE NUEVO", font=get_font(35), base_color="black", hovering_color="green")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(400, 350), 
-                            text_input="Salir", font=get_font(35), base_color="black", hovering_color="red")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(650, 540), 
+                            text_input="SALIR", font=get_font(35), base_color="black", hovering_color="red")
 
         CREDITOS = Button(image=None, pos=(400, 650),text_input="Gloria Gama, Jeyson Olivare, Julian", font=get_font(20), base_color="white", hovering_color="Green")
 
@@ -465,11 +468,11 @@ def main_menu():
         MENU_RECT = MENU_TEXT.get_rect(center=(450, 30))
 
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(450, 250), 
-                            text_input="Jugar", font=get_font(35), base_color="black", hovering_color="green")
+                            text_input="JUGAR", font=get_font(35), base_color="black", hovering_color="green")
         OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(450, 400), 
-                            text_input="Como jugar", font=get_font(35), base_color="black", hovering_color="green")
+                            text_input="CÓMO JUGAR", font=get_font(35), base_color="black", hovering_color="green")
         QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(450, 550), 
-                            text_input="Salir", font=get_font(35), base_color="black", hovering_color="red")
+                            text_input="SALIR", font=get_font(35), base_color="black", hovering_color="red")
 
         CREDITOS = Button(image=None, pos=(450, 650),text_input="Gloria Gama, Jeyson Olivare, Julian", font=get_font(20), base_color="white", hovering_color="Green")
 
